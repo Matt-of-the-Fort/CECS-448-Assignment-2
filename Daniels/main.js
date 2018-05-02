1fdaea4bed1103bf863e456dcceaa7c61824e159
dpan216 <dpan216@gmail.com>
2018-05-02 15:21:07 -0700

    Deleted unneccesary classes

--- Daniels/main.js
+++ Daniels/main.js
@@ -10,7 +10,6 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
     $('.dropdown-submenu .show').removeClass("show");
   });
 
-
   return false;
 });
 
@@ -32,17 +31,6 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
-  });
-
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
@@ -56,12 +44,4 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
-    $(".navbar").addClass("d-none");
-  })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
-    $(".navbar").removeClass("d-none");
-  })
-
-})(jQuery); // End of use strict
+})(jQuery);
